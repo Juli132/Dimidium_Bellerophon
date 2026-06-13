56282c954e6c922b9b8855607c68160775a8e8d4
Juli132 <jlmugiwara@gmail.com>
2026-06-12 22:52:01 -0400

    Merge branch 'main' of https://github.com/Juli132/Dimidium_Bellerophon

--- src/testers/Main.java
+++
@@ -1,9 +0,0 @@
-package testers;
-public class Main {
-    public static void main(String[] args) {
-        // the gui
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            new gui(); // launches window
-        });
-    }
-}
